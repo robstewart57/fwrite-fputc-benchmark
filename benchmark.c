57367168a3d4a729a4078aa7cab78d98ef756b2d
Rob Stewart <robstewart57@gmail.com>
2014-07-20 12:26:42 +0100

    Added two more benchmark cases

--- benchmark.c
+++ benchmark.c
@@ -30,26 +30,51 @@ void randChars() {
 int main(int argc, char **argv){
   clock_t begin, end;
   double time_spent;
+  FILE *fwrite_out_all;
   FILE *fwrite_out;
+  FILE *fwrite_out_fseek;
   FILE *fputc_out;
+  fwrite_out_all = fopen("fwrite-out-all.txt", "w+");
   fwrite_out = fopen("fwrite-out.txt", "w+");
+  fwrite_out_fseek = fopen("fwrite-out-fseek.txt", "w+");
   fputc_out  = fopen("fputc-out.txt", "w+");
   randChars();
 
   int i;
 
+  /* measure fwrite time, writing array at once */
+  begin = clock();
+  fwrite(chars, sizeof(chars[0]), sizeof(chars)/sizeof(chars[0]), fwrite_out_all);
+  fclose(fwrite_out_all);
+  end = clock();
+  time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  printf("fwrite time write all at once:\t %f\n",time_spent);
+
+
   /* measure fwrite time */
   static int cnt = 0;
   begin = clock();
   for (i=0; i < NUMCHARS; i++){
-    fseek(fwrite_out,sizeof(unsigned char)*cnt,SEEK_SET);
     fwrite(&chars[i],sizeof(unsigned char),1,fwrite_out);
     cnt++;
   }
   fclose(fwrite_out);
   end = clock();
   time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-  printf("fwrite time: %f\n",time_spent);
+  printf("fwrite time one byte at a time:\t %f\n",time_spent);
+
+  /* measure fwrite time, also using fseek */
+  cnt = 0;
+  begin = clock();
+  for (i=0; i < NUMCHARS; i++){
+    fseek(fwrite_out_fseek,sizeof(unsigned char)*cnt,SEEK_SET);
+    fwrite(&chars[i],sizeof(unsigned char),1,fwrite_out_fseek);
+    cnt++;
+  }
+  fclose(fwrite_out_fseek);
+  end = clock();
+  time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  printf("fwrite time one byte at a time:\t %f\n",time_spent);
 
   /* measure fputc time */
   begin = clock();
@@ -59,8 +84,7 @@ int main(int argc, char **argv){
   fclose(fputc_out);
   end = clock();
   time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-  printf("fputc time: %f\n",time_spent);
+  printf("fputc time one byte at a time:\t %f\n",time_spent);
 
   return 0;
 }
-
